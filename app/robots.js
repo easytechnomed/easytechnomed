9a720c313e63c0d11588b3b7be360225b329e8e7
A2D-mohd-mizna-ansari <mohd.ansari@a2deats.com>
2026-07-09 10:46:41 +0530

    fixed

--- app/robots.js
+++ app/robots.js
@@ -1,9 +1,17 @@
 export default function robots() {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: [
+          "GPTBot",
+          "ClaudeBot",
+          "Google-Extended",
+          "meta-externalagent",
+          "*",
+        ],
+        allow: "/",
+      },
+    ],
     sitemap: "https://easytechnomed.com/sitemap.xml",
   };
 }
