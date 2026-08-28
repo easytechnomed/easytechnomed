592d768a0b01f7d12793145417cbcc60f4b9ad7f
A2D-mohd-mizna-ansari <mohd.ansari@a2deats.com>
2026-08-28 16:54:12 +0530

    new ui added

--- components/ui/Input.js
+++ components/ui/Input.js
@@ -5,11 +5,10 @@ export const Input = forwardRef(({ className = "", error, ...props }, ref) => {
     <div className="w-full">
       <input
         ref={ref}
-        className={`w-full rounded-lg border-2 px-4 py-2.5 text-base md:text-sm transition-all focus:outline-none disabled:opacity-50 disabled:bg-slate-100 disabled:pointer-events-none bg-white text-slate-900 shadow-none
-          ${
-            error
-              ? "border-red-500 focus:border-red-600"
-              : "border-slate-300 focus:border-[#0f766e]"
+        className={`w-full rounded-lg border-2 px-4 py-2.5 text-sm transition-all focus:outline-none disabled:opacity-50 disabled:bg-slate-100 disabled:pointer-events-none bg-white text-slate-900 shadow-none
+          ${error
+            ? "border-red-500 focus:border-red-600"
+            : "border-slate-300 focus:border-[#0f766e]"
           } ${className}`}
         {...props}
       />
